730016084303e643e8c6cb8a24dfc55ce2a0f1dd
alex <alexandersegerev@gmail.com>
2024-09-24 13:12:50 -0400

    Put in Random Chosen Color

--- game.js
+++ game.js
@@ -1,6 +1,9 @@
+let buttonColors = ["red", "blue", "barash", "yellow"];
+
 function nextSequence() {
-  let randomNumber = Math.floor(Math.random() * 4) + 1;
-  console.log(randomNumber);
+  let randomNumber = Math.floor(Math.random() * 4);
+  let randomChosenColor = buttonColors[randomNumber];
+  console.log(randomChosenColor);
 }
 
 nextSequence();
