5f6cb48c61feaf2b9f591583ae1e1992e6eebba8
alex <alexandersegerev@gmail.com>
2024-09-24 13:15:05 -0400

    Added gamePattern array

--- game.js
+++ game.js
@@ -1,9 +1,13 @@
 let buttonColors = ["red", "blue", "barash", "yellow"];
+let gamePattern = [];
 
 function nextSequence() {
   let randomNumber = Math.floor(Math.random() * 4);
   let randomChosenColor = buttonColors[randomNumber];
-  console.log(randomChosenColor);
+  gamePattern.push(randomChosenColor);
+
+  //FOR TESTING PURPOSES -- DELETE AFTER PROJECT IS DONE
+  console.log(gamePattern);
 }
 
 nextSequence();
