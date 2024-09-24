4e442bb7c00990790ce3c6107652c52d7a423c16
alex <alexandersegerev@gmail.com>
2024-09-24 13:36:02 -0400

    Added button listener functionality

--- game.js
+++ game.js
@@ -1,12 +1,17 @@
 let buttonColors = ["red", "blue", "barash", "yellow"];
 let gamePattern = [];
+let userClickedPattern = [];
+
+$(".btn").click(function() {
+  alert("I got clicked!");
+})
 
 function nextSequence() {
   let randomNumber = Math.floor(Math.random() * 4);
   let randomChosenColor = buttonColors[randomNumber];
   gamePattern.push(randomChosenColor);
 
-  $("#" + randomChosenColor).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
+  $("#" + randomChosenColor).fadeIn(100).fadeOut(100).fadeIn(100);
 
   //FOR TESTING PURPOSES -- DELETE AFTER PROJECT IS DONE
   console.log(gamePattern);
