f4e2119dacbd6b0d887255ad0dd0f6282e6cc28b
alex <alexandersegerev@gmail.com>
2024-09-24 11:38:48 -0400

    Created a new pattern

--- game.js
+++ game.js
@@ -1,1 +1,11 @@
-alert("Hello!");
+let buttonColors = ["red", "blue", "yellow", "barash"];
+let gamePattern = [];
+
+function nextSequence() {
+    let randomNumber = Math.floor(Math.random() * 3);
+
+    let randomChosenColor = buttonColors(randomNumber);
+    
+    gamePattern.push(randomChosenColor);
+
+}
