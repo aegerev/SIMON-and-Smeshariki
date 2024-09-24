e6defb673afa9ac3278535fb826c11aa40b1f9c5
alex <alexandersegerev@gmail.com>
2024-09-24 15:12:55 -0400

    Added functionality to Start the Game

--- game.js
+++ game.js
@@ -2,33 +2,53 @@ let buttonColors = ["red", "blue", "barash", "yellow"];
 let gamePattern = [];
 let userClickedPattern = [];
 
+let level = 0;
+let started = false;
+
+$(document).keydown(function(event) {
+  if(!started) {
+    started = true;
+
+    $("#level-title").text("Level " + level);
+
+    nextSequence();
+    
+  }
+})
+
 $(".btn").click(function(event) {
   let userChosenColor = event.target.id;
   userClickedPattern.push(userChosenColor);
 
   animatePress(userChosenColor);
+
+  nextSequence();
   
-  //FOR TESTING PURPOSES -- DELETE AFTER PROJECT IS DONE
-  alert(userClickedPattern);
+  // //FOR TESTING PURPOSES -- DELETE AFTER PROJECT IS DONE
+  // alert(userClickedPattern);
 
 })
 
 
 function nextSequence() {
+  level++;
+  $("#level-title").text("Level " + level);
+  
   let randomNumber = Math.floor(Math.random() * 4);
   let randomChosenColor = buttonColors[randomNumber];
+
   gamePattern.push(randomChosenColor);
 
   $("#" + randomChosenColor).fadeIn(100).fadeOut(100).fadeIn(100);
 
-
+  
 }
 
 function animatePress(currentColor){
   $("#" + currentColor).addClass('pressed');
   setTimeout(function() {
     $("#" + currentColor).removeClass('pressed');
-  }, 200);
+  }, 100);
 }
 
 nextSequence();
